a7c534ecf096036a90847b5e4c8269b5fc2f3460
kcwww <cwkim0321@gmail.com>
2022-11-15 14:35:52 +0900

    libft strjoin itoa

--- libft/libft.h
+++ libft/libft.h
@@ -38,8 +38,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char* ft_strjoin(char const* s1, char const* s2);
-char* ft_strtrim(char const* s1, char const* set);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
 
 #endif
