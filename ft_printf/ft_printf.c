66bcc4e8c67f31c333eae3aa342d7ed51e553a35
kcwww <cwkim0321@gmail.com>
2022-11-22 14:02:10 +0900

    ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -12,8 +12,42 @@
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	check_format(const char *format, va_list ap)
+{
+	if (*format == 0)
+		return (-1);
+	if (*format == 'c')
+		ft_putchar_fd(va_arg(ap, int), 1);
+	else if (*format == 'd')
+		ft_putnbr_fd(va_arg(ap, int), 1);
+	else if (*format == 'x')
+		ft_puthex(va_arg(ap, int));
+	else if (*format == 'X')
+		ft_putBhex(va_arg(ap, int));
+	else if (*format == 's')
+		ft_putstr_fd(va_arg(ap, int), 1);
+	return (1);
+}
+
+int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-
+	
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			if(check_format(++format, ap) == -1)
+			{
+				va_end(ap);
+				return (-1);
+			}
+		}
+		else:
+			write(1, format, 1);
+		format++;
+	}
+	va_end(ap);
+	return (1);
 }
