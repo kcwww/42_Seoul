c791c06ed5913fc1a73908615d514493be0af853
kcwww <cwkim0321@gmail.com>
2023-08-24 21:49:57 +0900

    fix

--- philosophers/start.c
+++ philosophers/start.c
@@ -6,7 +6,7 @@
 /*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 15:24:00 by chanwoki          #+#    #+#             */
-/*   Updated: 2023/07/08 15:34:19 by chanwoki         ###   ########.fr       */
+/*   Updated: 2023/08/24 21:47:23 by chanwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	philo_start(t_philo *philo)
 	t_info	*info;
 
 	info = philo[0].info;
+	if (info->num_of_must_eat == 0)
+		return ;
 	if (info->num_of_philo % 2 == 0)
 		philo_even(philo);
 	else
