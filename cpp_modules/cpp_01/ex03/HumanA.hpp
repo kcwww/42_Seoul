ac7c6903dc9fd14a20496fb3564712ee60360f89
kcwww <cwkim0321@gmail.com>
2023-09-27 18:22:36 +0900

    make human class

--- cpp_modules/cpp_01/ex03/HumanA.hpp
+++ cpp_modules/cpp_01/ex03/HumanA.hpp
@@ -6,7 +6,24 @@
 /*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:03:28 by chanwoki          #+#    #+#             */
-/*   Updated: 2023/09/27 18:03:29 by chanwoki         ###   ########.fr       */
+/*   Updated: 2023/09/27 18:21:35 by chanwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+#include "Weapon.hpp"
+
+class HumanA
+{
+  private:
+    std::string	_name;
+    Weapon		&_weapon;
+  public:
+    HumanA(std::string name, Weapon &weapon);
+    ~HumanA(void);
+    void attack(void);
+};
+
+#endif
--- cpp_modules/cpp_01/ex03/HumanB.hpp
+++ cpp_modules/cpp_01/ex03/HumanB.hpp
@@ -6,7 +6,25 @@
 /*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:03:33 by chanwoki          #+#    #+#             */
-/*   Updated: 2023/09/27 18:03:34 by chanwoki         ###   ########.fr       */
+/*   Updated: 2023/09/27 18:22:07 by chanwoki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+#include "Weapon.hpp"
+
+class HumanB
+{
+  private:
+    std::string	_name;
+    Weapon		*_weapon;
+  public:
+    HumanB(std::string name);
+    ~HumanB(void);
+    void attack(void);
+    void setWeapon(Weapon &weapon);
+};
+
+#endif
