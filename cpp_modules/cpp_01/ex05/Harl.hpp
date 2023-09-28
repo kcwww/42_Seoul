f17bb947a2b2130f2881e8680462d0d791d53809
kcwww <cwkim0321@gmail.com>
2023-09-28 14:59:05 +0900

    cpp 01 ex05 temp

---
+++ cpp_modules/cpp_01/ex05/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanwoki <chanwoki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 14:55:47 by chanwoki          #+#    #+#             */
+/*   Updated: 2023/09/28 14:58:47 by chanwoki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+# include <iostream>
+
+class Harl
+{
+  public:
+    Harl(void);
+    ~Harl(void);
+    void complain(std::string level);
+  private:
+    void _debug(void);
+    void _info(void);
+    void _warning(void);
+    void _error(void);
+};
+
+#endif
